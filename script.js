03c3bbc60da6149dc7f0fa21da6d60db5b927a4d
Jorge Aguilar <aguila.techserv@gmail.com>
2020-03-19 18:18:49 -0500

    completedVariables

--- script.js
+++ script.js
@@ -1,55 +1,23 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+
 // global variables
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = ['!', '"', "#", "$", "%", "&", "'", '(', ')', '*', '+', ',', '-', '.', '/', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '{', '|', '}', '~' ];
 var lowerCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lengthOf = []
-for (var i = 8; i <= 128; i++) [
-  console.log(i)
-]
+var userPassLength = "";
+var concats = [];
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-  
- // below i am creating the variables for prompts
- function generatePassword() {}
-
-
-var userPassLength = prompt("How long will your password be? It has to be between 8 and 128");
+userPassLength = prompt("How long will your password be? It has to be between 8 and 128");
 console.log(userPassLength);
-if (userPassLength >= 8 <=128) 
-  console.log(generatePassword(lengthOf))
-else (userPassLength)
-  window.alert("Thank you");
-
-
-if (lowercase == true) {
-  generatePassword(lowerCharacters)
-} ;
-
-if (uppercase == true) {
-  generatePassword(upperCharacters)
-} ;
-
-if (numericValue == true) {
-  generatePassword(numericCharacters)
-} ;
-
-if (specialChar == true) {
-  generatePassword(specialCharacters)
-}
 var lowercase = confirm("Do you want lowercase?");
 console.log(lowercase);
 var uppercase = confirm("Do you want uppercase?");
@@ -59,11 +27,74 @@ console.log(numericValue);
 var specialChar = confirm("Do you want special characters?");
 console.log(specialChar);
 
-for (var index = 0; index < length; index++){
-    var character = Math.floor(Math.random() * (128 - 8))
-    password += userPassLength.substring(character, character + 1);
+// if (userPassLength >=8 && userPassLength <=128){}
+// else if (userPassLength <8) {
+// window.alert("password not long enough");
+// }
+// else {
+// window.alert("password too long")
+// }
+
+
+do {
   
 }
+while (userPassLength != (userPassLength >=8 && userPassLength <=128));
+
+
+
+if (lowercase == true) {
+   concats = concats.concat(lowerCharacters);
+ } 
+ else (lowercase == false)
+
+if (uppercase == true) {
+  concats = concats.concat(upperCharacters);
+} 
+else (uppercase == false)
+
+if (numericValue == true) {
+  concats = concats.concat(numericCharacters);
+} 
+else (numericValue == false)
+
+if (specialChar == true) {
+  concats = concats.concat(specialCharacters);
+}
+else (specialChar == false)
+
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+  
+ // below i am creating the variables for prompts
+ function generatePassword()  {
+var password = "";
+  for (var index = 0; index < parseInt(userPassLength); index++){
+    var character = Math.floor(Math.random() * concats.length)
+    password += concats[character];
+  }
+  console.log(password)
+  return(password)
+
+
+  
+ }
+
+
+
+
+
+
+
+
 
   
  
